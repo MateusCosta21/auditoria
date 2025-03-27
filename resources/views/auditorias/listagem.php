c9d889820183338e6eb6a65f18b232c4c1594d54
Mateus <joao.santos@fabricainfo.com>
2025-03-26 21:58:10 -0300

    [feat] aplicação
    menu dropdown ajustando ainda

---
+++ resources/views/auditorias/listagem.php
@@ -0,0 +1,9 @@
+<!-- resources/views/auditorias.blade.php -->
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Auditorias</h1>
+        <!-- Conteúdo de auditorias aqui -->
+    </div>
+@endsection
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,27 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <!-- Auditorias Dropdown -->
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                {{ __('Auditorias') }}
+                                <svg class="ml-1 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('auditorias.index')">
+                                {{ __('Listar Auditorias') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('auditorias.create')">
+                                {{ __('Cadastrar Auditoria') }}
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
                 </div>
             </div>
 
@@ -24,7 +45,6 @@
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
-
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -69,6 +89,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('auditorias.index')" :active="request()->routeIs('auditorias.index')">
+                {{ __('Listar Auditorias') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('auditorias.create')" :active="request()->routeIs('auditorias.create')">
+                {{ __('Cadastrar Auditoria') }}
             </x-responsive-nav-link>
         </div>
 
