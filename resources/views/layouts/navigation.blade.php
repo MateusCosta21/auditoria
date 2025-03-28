133b6b28a40cb1d9def3528250dd46201dc2ae7c
Mateus <joao.santos@fabricainfo.com>
2025-03-28 18:32:53 -0300

    [feat] Aplicação
    Oculta menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,11 +12,11 @@
 
     <!-- Navigation Links -->
     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex items-center">
-        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+        <!-- <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
             {{ __('Dashboard') }}
-        </x-nav-link>
+        </x-nav-link> -->
 
-        <!-- Auditorias Dropdown -->
+        <!-- Auditorias
         <x-dropdown align="right" width="48">
             <x-slot name="trigger">
                 <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
@@ -35,7 +35,7 @@
                     {{ __('Cadastrar Clientes') }}
                 </x-dropdown-link>
             </x-slot>
-        </x-dropdown>
+        </x-dropdown> Dropdown -->
 
         <x-dropdown align="right" width="48">
             <x-slot name="trigger">
